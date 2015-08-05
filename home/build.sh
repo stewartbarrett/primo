87c9ae3e69698829a0d2eeb9f84e957360ddcd8a
stewartbarrett <stewart.barrett@comparethemarket.com>
2015-08-05 12:08:37 +0100

    Build script for CodeDeploy

--- home/build.sh
+++ home/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
+#halts the script if there are errors
 set -e
-#set default region
+
+#set variables that you reference in GO pipeline ENVIRONMENT VARIABLES tab
 region="$REGION"
-
 application_name="$APPLICATION_NAME"
-
 deployment_group="$DEPLOYMENT_GROUP"
-
 s3_bucket="$S3_BUCKET"
 
+#creates the deploy push output (with variables defined in the GO pipeline) in the 'cmd' variable
 cmd=$(aws \
 --region "$region" \
 deploy push \
@@ -16,16 +17,22 @@ deploy push \
 --s3-location s3://"$s3_bucket"/"$application_name".zip \
 --ignore-hidden-files)
 
-#Strips text line off the output from the aws push command
+#strips inital text line from the output of the aws push
 cmd=$(echo "$cmd" | tail -n +2)
 
-#add region to command
+#adds the AWS 'region'
 cmd=$(echo "$cmd" | sed "s/aws deploy/aws --region $region deploy/")
+
+#adds the 'deployment group name'
 cmd=$(echo "$cmd" | sed "s/<deployment-group-name>/$deployment_group/")
 
+#strips out the 'description' field
 cmd=$(echo "$cmd" | sed "s/--description <description>//")
 
-# Create the deployment script
+#strips out the 'deployment config name'
+cmd=$(echo "$cmd" | sed "s/-'-deployment-config-name <deployment-config-name>//")
+
+# Creates the deployment script to run in the 'deploy' stage of the GO pipeline
 tee "deploy.sh" > /dev/null <<EOF
 #!/bin/bash
 set -e
